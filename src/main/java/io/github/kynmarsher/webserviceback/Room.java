63c24f443834976905d74f26a6dff6a7790ce250
Kynmarsher <104830743+Kynmarsher@users.noreply.github.com>
2022-05-05 23:00:27 +0500

    Нихуя не сделал (даун*2?)

---
+++ src/main/java/io/github/kynmarsher/webserviceback/Room.java
@@ -0,0 +1,9 @@
+package io.github.kynmarsher.webserviceback;
+
+public class Room {
+    public String roomId;
+    public Room(String id){
+        roomId = id;
+
+    }
+}
--- src/main/java/io/github/kynmarsher/webserviceback/WebServiceBack.java
+++ src/main/java/io/github/kynmarsher/webserviceback/WebServiceBack.java
@@ -1,12 +1,24 @@
 package io.github.kynmarsher.webserviceback;
+import java.util.HashMap;
+
 import static spark.Spark.*;
 
 public class WebServiceBack {
+   public HashMap<String, Room> roomList;
     public WebServiceBack(){
+        roomList=new HashMap<>();
         port(3100);
-        get("/hello", (request, response) -> {
-            System.out.println("Suck");
-            return "Cock";
+        get("/room/:roomid", (request, response) -> {
+            return roomList.get(request.params(":roomid")).roomId;
         });
+       post("/room", (request, response) -> {
+            roomList.put("123", new Room("sirop1234"));
+            return 200;
+        });
+       put("/room:roomid", (request, response)-> {
+
+        });
+
+
     }
 }
