bd6446bbe3a4318a7c9f8572aadbd42bef04ff96
realkarmakun <20980281+realkarmakun@users.noreply.github.com>
2022-05-31 08:38:42 +0600

    json moment

--- src/main/java/io/github/kynmarsher/webserviceback/socketio/webrtc/CreateOfferPacket.java
+++ src/main/java/io/github/kynmarsher/webserviceback/socketio/webrtc/CreateOfferPacket.java
@@ -1,4 +1,7 @@
 package io.github.kynmarsher.webserviceback.socketio.webrtc;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"roomId", "offerFrom", "offerBody"})
 public record CreateOfferPacket(String roomId, String offerFrom, String offerBody) {
 }
