d91dea96fb4f16141bd4dbb0980faa65706356f8
realkarmakun <20980281+realkarmakun@users.noreply.github.com>
2022-06-02 08:44:52 +0600

    JsonIgnore

--- src/main/java/io/github/kynmarsher/webserviceback/socketio/webrtc/CreateOfferPacket.java
+++ src/main/java/io/github/kynmarsher/webserviceback/socketio/webrtc/CreateOfferPacket.java
@@ -1,7 +1,13 @@
 package io.github.kynmarsher.webserviceback.socketio.webrtc;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({"roomId", "offerFrom", "offerBody"})
 public record CreateOfferPacket(String roomId, String offerFrom, String offerBody) {
+
+    @JsonIgnore
+    public String offerBody() {
+        return this.offerBody;
+    }
 }
