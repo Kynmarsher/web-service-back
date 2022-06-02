6ddf3fe13c6e7cb9418666dc086cc902572e3d8e
realkarmakun <20980281+realkarmakun@users.noreply.github.com>
2022-06-02 20:13:16 +0600

    Debugging

--- src/main/java/io/github/kynmarsher/webserviceback/socketio/chat/IncomingChatMessagePacket.java
+++ src/main/java/io/github/kynmarsher/webserviceback/socketio/chat/IncomingChatMessagePacket.java
@@ -1,4 +1,7 @@
 package io.github.kynmarsher.webserviceback.socketio.chat;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"roomId", "userId", "message"})
 public record IncomingChatMessagePacket(String roomId, String userId, String message) {
 }
