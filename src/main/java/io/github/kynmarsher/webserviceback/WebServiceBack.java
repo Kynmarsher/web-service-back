cc8db1ed338fc209aae372eac64a1de1bc46218b
realkarmakun <20980281+realkarmakun@users.noreply.github.com>
2022-06-02 20:18:30 +0600

    Debugging

--- src/main/java/io/github/kynmarsher/webserviceback/WebServiceBack.java
+++ src/main/java/io/github/kynmarsher/webserviceback/WebServiceBack.java
@@ -225,7 +225,8 @@ public class WebServiceBack {
             socket.on("chatMessage", msgArgs -> {
                 try {
                     final var chatMsg = WebServiceBack.STRICT_MAPPER.readValue(msgArgs[0].toString(), IncomingChatMessagePacket.class);
-                    socket.send("chatMessageReceive", msgArgs[0]);
+                    socket.send("chatMessage", msgArgs[0]);
+                    socket.broadcast(chatMsg.roomId(), "chatMessage", msgArgs[0]);
                     System.out.println(chatMsg);
                     /*if ( chatMsg.message() != null && chatMsg.message().length() <= 128 ) {
                         *//*socket.send( "chatMessage", msgArgs[0]);
