532e543b5e21c076c2bfb0652601ddd48fa1f1b1
realkarmakun <20980281+realkarmakun@users.noreply.github.com>
2022-06-13 02:58:19 +0600

    Admin stuff

--- src/main/java/io/github/kynmarsher/webserviceback/socketio/room/GenericAnswerPacket.java
+++ src/main/java/io/github/kynmarsher/webserviceback/socketio/room/GenericAnswerPacket.java
@@ -1,5 +1,3 @@
 package io.github.kynmarsher.webserviceback.socketio.room;
 
-public record GenericAnswerPacket(boolean status, String userId, String message) {
-
-}
+public record GenericAnswerPacket(boolean status, String message) { }
